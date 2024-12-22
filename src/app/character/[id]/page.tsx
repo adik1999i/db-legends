7553e279445a93eee92829f1a2de024a1ed08555
adik1999i <adityak1999i@gmail.com>
2024-12-22 21:51:02 +0530

    new

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,55 +1,56 @@
+/* eslint-disable @typescript-eslint/await-thenable */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import CharacterDetail from '../../../components/Character';
 import { ALL_CHARACTERS, CharacterId } from '../../../data/characters';
 import { notFound } from 'next/navigation';
 import { Metadata } from 'next';
 
-
 function isValidCharacterId(id: string): id is CharacterId {
-    return id in ALL_CHARACTERS;
-  }
+  return id in ALL_CHARACTERS;
+}
+
+type PageProps = {
+  params: { id: string } | Promise<{ id: string }>;
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+// @ts-ignore
+export async function generateMetadata({ 
+  params 
+}: PageProps): Promise<Metadata> {
+  const resolvedParams = await params;
   
-  // Define params interface
-  type PageProps = {
-    params: { id: string } | Promise<{ id: string }>;
-    searchParams: { [key: string]: string | string[] | undefined };
-  };
-  
-  // Metadata generation
-  export async function generateMetadata({ 
-    params 
-  }: PageProps): Promise<Metadata> {
-    const resolvedParams = await params;
-    
-    if (!isValidCharacterId(resolvedParams.id)) {
-      return {
-        title: 'Character Not Found - DB Legends Codex'
-      };
-    }
-  
-    const character = ALL_CHARACTERS[resolvedParams.id];
+  if (!isValidCharacterId(resolvedParams.id)) {
     return {
-      title: `${character.name} - DB Legends Codex`,
-      description: character.title
+      title: 'Character Not Found - DB Legends Codex'
     };
   }
+
+  const character = ALL_CHARACTERS[resolvedParams.id];
+  return {
+    title: `${character.name} - DB Legends Codex`,
+    description: character.title
+  };
+}
+
+// @ts-ignore
+export default async function Page({ 
+  params 
+}: PageProps) {
+  const resolvedParams = await params;
   
-  // Page component
-  export default async function Page({ 
-    params 
-  }: PageProps) {
-    const resolvedParams = await params;
-    
-    if (!isValidCharacterId(resolvedParams.id)) {
-      notFound();
-    }
-  
-    const character = ALL_CHARACTERS[resolvedParams.id];
-    return <CharacterDetail character={character} />;
+  if (!isValidCharacterId(resolvedParams.id)) {
+    notFound();
   }
-  
-  // Static params generation for optimization
-  export function generateStaticParams() {
-    return Object.keys(ALL_CHARACTERS).map((id) => ({
-      id
-    }));
-  }
+
+  const character = ALL_CHARACTERS[resolvedParams.id];
+  return <CharacterDetail character={character} />;
+}
+
+export function generateStaticParams() {
+  return Object.keys(ALL_CHARACTERS).map((id) => ({
+    id
+  }));
+}
